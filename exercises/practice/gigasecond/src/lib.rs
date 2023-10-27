590c7f87b60f041b07d80164a39063e39905adf8
Pierre Barbera <pierre.barbera@mobilityhouse.com>
2023-10-27 14:59:22 +0200

    first three exercises

--- exercises/practice/gigasecond/src/lib.rs
+++ exercises/practice/gigasecond/src/lib.rs
@@ -2,5 +2,10 @@ use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    // todo!("What time is a gigasecond later than {start}");
+    let result = start + time::Duration::SECOND * 1e9;
+    println!("start: {start}");
+    println!("after gigaseconds: {result} \n");
+    return result;
+
 }
